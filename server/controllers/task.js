cad738bb0a7117edfd0f88b0ff8ad26893898699
Mavai <marko.vainio@helsinki.fi>
2018-09-25 22:03:28 +0300

    Task update and delete to REST API

--- server/controllers/task.js
+++ server/controllers/task.js
@@ -16,9 +16,32 @@ taskRouter.post('/', async (request, response) => {
     response.status(201).json(Task.format(savedTask));
   } catch(excpetion) {
     console.warn(excpetion);
-    response.status(500).json({ error: 'Something went wrong, check the console for error messages.' });
+    response.status(500).json({ error: 'Something went wrong when creating a task' });
   }
 
+});
+
+taskRouter.put('/:id', async (request, response) => {
+  try {
+    const body = request.body;
+    const task = { ...body };
+    const updatedTask = await Task
+      .findByIdAndUpdate(request.params.id, task, { new: true });
+    response.status(203).json(Task.format(updatedTask));
+  } catch (excpetion) {
+    console.warn(excpetion);
+    response.status(500).json({ error: 'Something went wrong when updating a task.' });
+  }
+});
+
+taskRouter.delete('/:id', async (request, response) => {
+  try {
+    const deletedTask = await Task.findByIdAndRemove(request.params.id);
+    response.status(204).json(Task.format(deletedTask));
+  } catch (excpetion) {
+    console.warn(excpetion);
+    response.status(500).json({ error: 'Something went wrong when deletinig a task' });
+  }
 });
 
 module.exports = taskRouter;
