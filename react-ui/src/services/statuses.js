55aceb0814d658a93541318df3e26ef57ed17515
Marko Vainio <marko.vainio@helsinki.fi>
2018-11-05 20:50:56 +0200

    Removed ui to separate repo

--- react-ui/src/services/statuses.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-const baseUrl = '/api/statuses';
-
-const getAll = async () => {
-  const response = await axios.get(baseUrl);
-  return response.data;
-};
-
-export default { getAll };
