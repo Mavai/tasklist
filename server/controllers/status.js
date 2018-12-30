10f06d57d97b25899ae907b0c1cfd82ac30466fd
Marko Vainio <marko.vainio@helsinki.fi>
2018-12-30 17:18:37 +0200

    Initial statuses

--- server/controllers/status.js
+++ server/controllers/status.js
@@ -1,8 +1,32 @@
 const statusRouter = require('express').Router();
 const Status = require('../models/Status');
 
+const initialStatuses = [
+  {
+    name: 'Not started',
+    color: 'gray'
+  },
+  {
+    name: 'Started',
+    color: 'red'
+  },
+  {
+    name: 'In review',
+    color: 'cyan'
+  },
+  { name: 'Ready', color: 'lightGreen' }
+];
+
 statusRouter.get('/', async (request, response) => {
-  const statuses = await Status.find({});
+  let statuses = await Status.find({});
+
+  if (statuses.length === 0) {
+    const newStatuses = initialStatuses.map(status => {
+      Status.create(status);
+    });
+    await Promise.all(newStatuses);
+    statuses = await Status.find({});
+  }
 
   response.json(statuses.map(Status.format));
 });
