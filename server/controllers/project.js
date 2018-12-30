85b0dc8cb569efe164b652f2346694ff346c3f56
Marko Vainio <marko.vainio@helsinki.fi>
2018-12-30 14:48:41 +0200

    Heroku init.

--- server/controllers/project.js
+++ server/controllers/project.js
@@ -7,13 +7,30 @@ projectRouter.get('/', async (request, response) => {
   response.json(projects.map(Project.format));
 });
 
+projectRouter.post('/', async (request, response) => {
+  try {
+    const body = request.body;
+    if (body.name === undefined) {
+      return response.status(400).json({ error: 'Name missing' });
+    }
+    const project = new Project({ ...body });
+    await project.save();
+    response.status(201).json(Project.format(project));
+  } catch (excpetion) {
+    console.warn(excpetion.message);
+    response
+      .status(500)
+      .json({ error: 'Something went wrong when creating a project.' });
+  }
+});
+
 projectRouter.put('/:id', async (request, response) => {
   try {
     const body = request.body;
     if (body.name === undefined) {
       return response.status(400).json({ error: 'Name missing' });
     }
-    const project = { ...body, test: 'test' };
+    const project = { ...body };
     const updatedProject = await Project.findOneAndUpdate(
       { _id: request.params.id },
       project,
--- server/controllers/status.js
+++ server/controllers/status.js
@@ -1,9 +1,31 @@
 const statusRouter = require('express').Router();
 const Status = require('../models/Status');
 
-statusRouter.get('/', async (request, response) => {
-  const statuses = await Status.find({});
+const initialStatuses = [
+  {
+    name: 'Not started',
+    color: 'gray'
+  },
+  {
+    name: 'Started',
+    color: 'red'
+  },
+  {
+    name: 'In review',
+    color: 'cyan'
+  },
+  { name: 'Ready', color: 'lightGreen' }
+];
 
+statusRouter.get('/', async (request, response) => {
+  let statuses = await Status.find({});
+
+  if (statuses.length !== 4) {
+    await Status.remove({});
+    const promisearray = initialStatuses.map(status => Status.create(status));
+    await Promise.all(promisearray);
+    statuses = await Status.find({});
+  }
   response.json(statuses.map(Status.format));
 });
 
